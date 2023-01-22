64bc2ddc547636e13c574f081600f4f5757fb9fd
Elena <panovaelena1@yandex.ru>
2023-01-23 00:09:05 +0300

    Added Task2

---
+++ TASK2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int numb = Convert.ToInt32 (Console.ReadLine ());
+if (numb>99)
+{
+    int MakeArray = numb;
+    int digitIndex = 2;
+    int digit = numb.ToString()[digitIndex] - '0';
+    System.Console.WriteLine(digit);
+}
+else 
+{
+    System.Console.WriteLine("Третьей цифры нет");
+    
+}
